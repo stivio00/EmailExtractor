2e72c2b191fcaf1b517f59cc09817534f708add0
Stephen Krol <krol17@gmail.com>
2020-03-28 00:24:48 -0500

    Update main.js

--- main.js
+++ main.js
@@ -3,24 +3,49 @@
 function procesar_datos(){
     var input_element = document.getElementById("table_input");
     var output_element = document.getElementById("table_output");
+    var solo_inclusion = document.getElementById("solo_inclusion");
 
     console.log("test");
     var texto  = input_element.value;
     output_element.value = "";
     var lista = texto.split("\n");
     console.log(lista);
+
+    var count = 0;
     lista.forEach((e,i) => {
-        output_element.value += e.split("\t")[7] + ";";
+        //0:id, 1:cedula , 2:apellido, nombre 3:tipo matricula(Normal, Inclusión)
+        let cols = e.split("\t"); 
+        if (cols[7] != undefined) {
+            if (solo_inclusion.checked && cols[3] == "Inclusión"){
+                //solo los que estan incluyendo
+                output_element.value += cols[7] + "; ";
+                count++;
+            } else if (!solo_inclusion.checked) {
+                //Agregamos todos
+                output_element.value += cols[7]+ "; ";
+                count++;
+            } else {
+                console.log("error");
+            }
+        }
     });
 
-    copyToClipboard();
+    var label = document.getElementById("correos_label");
+    if (count != 0) {
+        label.innerHTML= `Correos procesados (${count}):`;
+        copyToClipboard();
+    }  else {
+        label.innerHTML = "Correos (0) : "
+        postMessage("No se encontraron correos");
+    }
 }
 
 function pasteFromClipboard() {
     console.log("pasted!!");
-    var copyText = document.getElementById("table_input");
-    copyText.focus();
-    document.execCommand("paste");
+    navigator.clipboard.readText().then(clipText => {
+        document.getElementById("table_input").value = clipText;
+        postMessage("Pegado");
+    });
 }
 
 function copyToClipboard() {
@@ -28,14 +53,22 @@ function copyToClipboard() {
   copyText.select();
   copyText.setSelectionRange(0, 99999); /*For mobile devices*/
   document.execCommand("copy");
-  alert(`Listo copiado. Por favor pegar contenido en el sujerto del correo`);
+  postMessage(`Listo, el resultado fue copiado. \nPor favor pegar el contenido en el sujeto del correo.`);
 }
 
 function limpiar(element) {
     if(element === 1){
-        document.getElementById("table_input").value="";
+        document.getElementById("table_input").value = "";
+
     }else{
-        document.getElementById("table_output").value="";
+        document.getElementById("table_output").value = "";
     }
-    
+    //postMessage("limpio");
+}
+
+function postMessage(text) {
+    var x = document.getElementById("snackbar");
+    x.className = "show";
+    x.innerHTML = text;
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
 }
